95bc8c387ca2e32f6e76b2a467511f7550f1bae2
Pavanreddy837 <siddireddypavan837@gmail.com>
2026-04-15 22:01:44 +0530

    Update python_basics_S Pavan veera shankar reddy .py

--- python_basics_S Pavan veera shankar reddy .py
+++ python_basics_S Pavan veera shankar reddy .py
@@ -1,1 +1,14 @@
-
+name = "Alex"
+age = 25
+height =1.75
+is_student = True
+print(name, age, height, is_student)
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+age_in_months = age*12
+age_in_days = age*365
+remainder_by_7 = age%7
+age_squared = age**2
+print(f"Age in months: {age_in_months}")
+print(f"Age in days: {age_in_days}")
+print(f"Remainder when age is divided by 7: {remainder_by_7}")
+print(f"Age raised to the power of 2: {age_squared}")
